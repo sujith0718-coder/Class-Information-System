612392cc6873be5477c1390a19a82715b59c67a8
sujith0718-coder <sujith0718@gmail.com>
2026-06-28 23:31:11 +0530

    updated

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+var announcements = [
+    {
+        title: "DSP Hall Changed",
+        priority: "Critical"
+    }
+    ,
+    {
+        title: "Record Submission Tomorrow",
+        priority: "Important"
 
+    }
+    ,
+    {
+        title: "Faculty Leave",
+        priority: "Information"
+    }
+
+];
+
+var announcementList = document.getElementById("announcements-list");
+announcements.forEach(function (announcement) {
+    announcementList.innerHTML += `<div class="announcement-card">
+        <h3>${announcement.title}</h3>
+        <p>${announcement.priority}</p>
+    </div>`
+
+
+});
